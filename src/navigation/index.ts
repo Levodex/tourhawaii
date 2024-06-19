49db2b228ae365d9a2b798714e7e3fde21b22d91
Ash <ashuto.sh@outlook.com>
2024-06-19 21:32:18 +0530

    Navigation setup

---
+++ src/navigation/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Bottom Tab Navigator for our App
+ */
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const TabNavigator = createBottomTabNavigator();
+
+export default TabNavigator;
