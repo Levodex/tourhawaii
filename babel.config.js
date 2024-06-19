e4d25499657e10bd3313d78955de5b9291b2ffe4
Ash <ashuto.sh@outlook.com>
2024-06-19 20:29:14 +0530

    Path alias and decorators

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,22 @@
 /* eslint-disable */
 
-module.exports = {
-    presets: ['module:@react-native/babel-preset'],
+/**
+ * Custom babel config with path alias and decorators enabled
+ */
+module.exports = function (api) {
+    api.cache(false);
+    return {
+        presets: ['module:@react-native/babel-preset'],
+        plugins: [
+            [
+                'module-resolver',
+                {
+                    root: ['./src/'],
+                    alias: {
+                        '@': './src/',
+                    },
+                },
+            ],
+        ],
+    };
 };
--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,33 @@
 /* eslint-disable */
 
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
 /**
- * Metro configuration
+ * Custom Metro configuration with path aliases and decorators enabled
+ *
  * https://reactnative.dev/docs/metro
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+    resolver: {
+        extraNodeModules: new Proxy(
+            {},
+            {
+                get: (target, name) => {
+                    if (target[name]) {
+                        return target[name];
+                    }
+                    // Enabling decorators
+                    if (name.startsWith('@')) {
+                        return path.join(__dirname, `src/${name.slice(1)}`);
+                    }
+                    return path.join(__dirname, `node_modules/${name}`);
+                },
+            },
+        ),
+    },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
